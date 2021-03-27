6bc797dc7f7e2b3c5b6609bb61078aa9777db9df
w65l76 <host_master@mail.ru>
2021-03-27 19:05:01 +0300

    заглушка получения узла

--- web/app.py
+++ web/app.py
@@ -31,6 +31,33 @@ def browse():
     return render_template('browse.html', node=node)
 
 
+@app.route('/node', methods=['GET'])
+def search():
+    node = request.args.get('node')
+
+    result = dict(
+        node=dict(url=f'browse?node={node}', title=f'Статья про {node}', text='На каждый {node} найдётся анти-{node}'),
+        parents=[
+            dict(
+                url='browse?node={node}_parent_{i}',
+                title='Статья про родителя {node} №{i}',
+                head='Одной из абстракций {node} является следующий узел.')
+            for i in range(10)],
+        children=[
+            dict(
+                url='browse?node={node}_child_{i}',
+                title='Статья про потомка {node} №{i}',
+                head='Одним из частных случаев {node} является следующий узел.')
+            for i in range(10)]
+    )
+
+    if query is not None:
+        pass
+
+    return render_template('search.html', search=search)
+
+
+
 @app.route('/')
 def index():
     return redirect(url_for('browse'))
