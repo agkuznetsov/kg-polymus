90f1d6488631876ebf7af15403fa19ead7cb2fb1
w65l76 <host_master@mail.ru>
2021-03-27 19:10:39 +0300

    fix

--- web/app.py
+++ web/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, url_for, redirect, request
 
 from web import api
 
+import json
+
 app = Flask(__name__)
 app.register_blueprint(api.bp)
 
@@ -36,22 +38,22 @@ def node():
     _node = request.args.get('node')
 
     result = dict(
-        node=dict(url=f'browse?node={_node}', title=f'Статья про {_node}', text='На каждый {node} найдётся анти-{_node}'),
+        node=dict(url=f'browse?node={_node}', title=f'Статья про {_node}', text=f'На каждый {_node} найдётся анти-{_node}'),
         parents=[
             dict(
-                url='browse?node={_node}_parent_{i}',
-                title='Статья про родителя {_node} №{i}',
-                head='Одной из абстракций {_node} является следующий узел.')
+                url=f'browse?node={_node}_parent_{i}',
+                title=f'Статья про родителя {_node} №{i}',
+                head=f'Одной из абстракций {_node} является следующий узел.')
             for i in range(10)],
         children=[
             dict(
-                url='browse?node={_node}_child_{i}',
-                title='Статья про потомка {_node} №{i}',
-                head='Одним из частных случаев {_node} является следующий узел.')
+                url=f'browse?node={_node}_child_{i}',
+                title=f'Статья про потомка {_node} №{i}',
+                head=f'Одним из частных случаев {_node} является следующий узел.')
             for i in range(10)]
     )
 
-    return result
+    return json.dumps(result, ensure_ascii=False, indent=4)
 
 
 
